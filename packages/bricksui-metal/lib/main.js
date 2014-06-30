1fc83eb2f41a5ba57f6ba79aafb03097b922024a
Nav4e <yodelinping@gmail.com>
2014-06-30 09:05:31 +0800

    comment core module

--- packages/bricksui-metal/lib/main.js
+++ packages/bricksui-metal/lib/main.js
@@ -1,13 +1,13 @@
 import BricksUI from 'bricksui-metal/core';
-import EventManager from 'bricksui-metal/event_manager';
-import StateHandler from 'bricksui-metal/state_handler';
-import Stateable from 'bricksui-metal/stateable';
-import swapHelpers from 'bricksui-metal/helper-support';
+//import EventManager from 'bricksui-metal/event_manager';
+//import StateHandler from 'bricksui-metal/state_handler';
+//import Stateable from 'bricksui-metal/stateable';
+//import swapHelpers from 'bricksui-metal/helper-support';
 /**
  * @module bricksui
  * @submodule bricksui-metal
  */
-BricksUI.EventManager = EventManager;
-BricksUI.Stateable = Stateable;
-BricksUI.StateHandler = StateHandler;
+//BricksUI.EventManager = EventManager;
+//BricksUI.Stateable = Stateable;
+//BricksUI.StateHandler = StateHandler;
 BricksUI.swapHelpers = swapHelpers;
